85ce01f9861aa4a86ca749ced244ccd39906db90
Ana Luiza <analuizaramalho9@gmail.com>
2020-11-13 12:31:54 -0300

    adicionando o bundle

--- DigitalHouseFoods/app/src/main/java/com/example/digitalhousefoods/menuListFoods/view/MenuListAdapter.kt
+++ DigitalHouseFoods/app/src/main/java/com/example/digitalhousefoods/menuListFoods/view/MenuListAdapter.kt
@@ -4,26 +4,35 @@ import android.view.LayoutInflater
 
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.digitalhousefoods.R
 import com.example.digitalhousefoods.menuListFoods.model.Restaurantes
+import com.squareup.picasso.Picasso
 
-class MenuListAdapter(private val dataSet: List<Restaurantes>) :
+
+class MenuListAdapter(
+    private val dataSet: List<Restaurantes>,
+    private val clickListener: (Restaurantes) -> Unit
+) :
     RecyclerView.Adapter<MenuListAdapter.MenuListViewHolder>() {
 
     class MenuListViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         private val nome: TextView = view.findViewById(R.id.txtNomeRestaurante)
         private val endereco: TextView = view.findViewById(R.id.txtEndereco)
         private val funcionamento: TextView = view.findViewById(R.id.txtFuncionamento)
+        private val image :ImageView=view.findViewById(R.id.imageMenu)
 
         fun bind(restaurantes: Restaurantes) {
 
             nome.text = restaurantes.nome
             endereco.text = restaurantes.endereco
             funcionamento.text = restaurantes.funcionamento
+            Picasso.get()
+                .load(restaurantes.imageUrl)
+                .into(image)
         }
-
     }
 
     override fun onCreateViewHolder(
@@ -37,6 +46,7 @@ class MenuListAdapter(private val dataSet: List<Restaurantes>) :
 
     override fun onBindViewHolder(holder: MenuListAdapter.MenuListViewHolder, position: Int) {
         holder.bind(dataSet[position])
+        holder.itemView.setOnClickListener{clickListener(dataSet[position])}
     }
 
     override fun getItemCount() = dataSet.size
--- DigitalHouseFoods/app/src/main/java/com/example/digitalhousefoods/menuListFoods/view/MenuListFragment.kt
+++ DigitalHouseFoods/app/src/main/java/com/example/digitalhousefoods/menuListFoods/view/MenuListFragment.kt
@@ -5,21 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
-import android.widget.TextView
+import androidx.core.os.bundleOf
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.digitalhousefoods.R
 import com.example.digitalhousefoods.menuListFoods.model.Restaurantes
 import com.example.digitalhousefoods.menuListFoods.repository.MenuListRepository
 import com.example.digitalhousefoods.menuListFoods.viewmodel.MenuListViewModel
+import com.google.android.material.card.MaterialCardView
 
 
 class MenuListFragment : Fragment() {
     lateinit var menuView: View
     lateinit var viewModel: MenuListViewModel
+    lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,6 +33,11 @@ class MenuListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         menuView = inflater.inflate(R.layout.fragment_menu_list, container, false)
+        return menuView
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
         viewModel = ViewModelProvider(
             this,
@@ -41,13 +49,19 @@ class MenuListFragment : Fragment() {
         })
         viewModel.obterRestaurantes()
 
-        return menuView
+
     }
 
     private fun criarLista(lista: List<Restaurantes>) {
+        navController = Navigation.findNavController(menuView)
+
         val viewManager = LinearLayoutManager(menuView.context)
         val recyclerView = menuView.findViewById<RecyclerView>(R.id.minhaLista)
-        val menuAdapter = MenuListAdapter(lista)
+        val menuAdapter = MenuListAdapter(lista) {
+
+            val bundle = bundleOf(NOME_RESTAURANTE to it.nome, IMAGEM_RESTAURANTE to it.imageUrl)
+            navController.navigate(R.id.DetalheRestauranteFragment, bundle)
+        }
 
         recyclerView.apply {
             setHasFixedSize(true)
@@ -56,21 +70,11 @@ class MenuListFragment : Fragment() {
         }
     }
 
-/*    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-
-        viewModel=ViewModelProvider(
-            this,
-            MenuListViewModel.MenuListViewModelFactory(MenuListRepository())
-        ).get(MenuListViewModel::class.java)
-
-        viewModel.listaRestaurantesData.observe(viewLifecycleOwner, Observer {
-            menuView.findViewById<TextView>(R.id.txtNomeRestaurante).text = it[0].nome
-        })
-    }*/
-
     companion object {
         @JvmStatic
         fun newInstance() = MenuListFragment()
+
+        val NOME_RESTAURANTE = "NOME"
+        val IMAGEM_RESTAURANTE = "IMAGEM"
     }
 }
