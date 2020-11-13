e025e1caf237b5249c87f2500b90c83f481fef5d
Ana Luiza <analuizaramalho9@gmail.com>
2020-11-13 12:37:10 -0300

    configurações layouts

--- DigitalHouseFoods/app/src/main/java/com/example/digitalhousefoods/login/view/LoginFragment.kt
+++ DigitalHouseFoods/app/src/main/java/com/example/digitalhousefoods/login/view/LoginFragment.kt
@@ -6,11 +6,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.navigation.Navigation
 import androidx.navigation.fragment.findNavController
 import com.example.digitalhousefoods.R
+import kotlinx.android.synthetic.main.fragment_login.*
 
 class LoginFragment : Fragment() {
+    lateinit var loginView: View
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,25 +24,30 @@ class LoginFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-      return inflater.inflate(R.layout.fragment_login, container, false)
+        loginView = inflater.inflate(R.layout.fragment_login, container, false)
+        return loginView
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val navController= Navigation.findNavController(view)
+        val navController = Navigation.findNavController(view)
 
-        view.findViewById<Button>(R.id.btnRegister_loginFragment).setOnClickListener{
+        view.findViewById<Button>(R.id.btnRegister_loginFragment).setOnClickListener {
             navController.navigate(R.id.registerFragment)
         }
 
-        view.findViewById<Button>(R.id.btnLogin_loginFragment).setOnClickListener{
-            navController.navigate(R.id.menuListFragment)
-        }
+        view.findViewById<Button>(R.id.btnLogin_loginFragment).setOnClickListener {
+                navController.navigate(R.id.menuListFragment)
+            }
     }
+
+
     companion object {
         @JvmStatic
-        fun newInstance() =   LoginFragment()
+        fun newInstance() = LoginFragment()
     }
 }
+
+
+
