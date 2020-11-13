74344603cbc1857dce6d1094013eabdd8491bfa2
Ana Luiza <analuizaramalho9@gmail.com>
2020-11-12 23:42:09 -0300

    Exibindo imagens nos Cards do Menu Restaurante

--- DigitalHouseFoods/app/src/main/java/com/example/digitalhousefoods/menuListFoods/model/Restaurantes.kt
+++ DigitalHouseFoods/app/src/main/java/com/example/digitalhousefoods/menuListFoods/model/Restaurantes.kt
@@ -1,4 +1,4 @@
 package com.example.digitalhousefoods.menuListFoods.model
 
 
-data class Restaurantes(val nome:String, val endereco:String,val funcionamento:String)
+data class Restaurantes(val nome:String, val endereco:String,val funcionamento:String,val imageUrl:Int)
--- DigitalHouseFoods/app/src/main/java/com/example/digitalhousefoods/menuListFoods/repository/MenuListRepository.kt
+++ DigitalHouseFoods/app/src/main/java/com/example/digitalhousefoods/menuListFoods/repository/MenuListRepository.kt
@@ -1,6 +1,7 @@
 package com.example.digitalhousefoods.menuListFoods.repository
 
 import android.content.Context
+import com.example.digitalhousefoods.R
 import com.example.digitalhousefoods.menuListFoods.model.Restaurantes
 
 class MenuListRepository(private val context: Context) {
@@ -10,22 +11,22 @@ class MenuListRepository(private val context: Context) {
                 Restaurantes(
                     "Tony Roma's",
                     "Av. Lavandisca,717 - Indianópolis, São Paulo",
-                    "Fecha às 22:00"
+                    "Fecha às 22:00", R.drawable.restaurante1
                 ),
                 Restaurantes(
                     "Aoyoma - Moema",
                     "Alameda dos Arapanés, 532 - Moema",
-                    "Fecha às 00:00"
+                    "Fecha às 00:00",R.drawable.restaurante2
                 ),
                 Restaurantes(
                     "Outback - Moema",
                     "Av. Moaci, 187, 187 - Moema, São Paulo",
-                    "Fecha às 00:00"
+                    "Fecha às 00:00",R.drawable.restaurante3
                 ),
                 Restaurantes(
                     "Sí Señor!",
                     "Alameda Jauaperi, 626 - Moema",
-                    "Fecha às 01:00"
+                    "Fecha às 01:00",R.drawable.restaurante4
                 )
             )
         )
