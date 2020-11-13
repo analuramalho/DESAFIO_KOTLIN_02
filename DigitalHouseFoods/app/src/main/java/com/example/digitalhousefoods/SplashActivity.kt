5f264e0be6b6941f35d17a7913f630fcd94a8975
Ana Luiza <analuizaramalho9@gmail.com>
2020-11-12 22:38:32 -0300

    Splash Screen

---
+++ DigitalHouseFoods/app/src/main/java/com/example/digitalhousefoods/SplashActivity.kt
@@ -0,0 +1,24 @@
+package com.example.digitalhousefoods
+
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowInsets
+import android.view.WindowManager
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 4000)
+
+    }
+}
